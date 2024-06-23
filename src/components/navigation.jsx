bdaac78b92ec93444fbf0804995aa876b787144d
victorsamsonov <vsamsonov@hawk.iit.edu>
2024-06-23 14:04:32 -0500

    Revert "theme"
    This reverts commit cbe2bf5c305f8fd47c2212d47d6aaf035d40f19a.

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -17,7 +17,7 @@ export const Navigation = (props) => {
             <span className="icon-bar"></span>{" "}
             <span className="icon-bar"></span>{" "}
           </button>
-          <a className="navbar-brand page-scroll" href="#page-top" style={{color: "#F9B516"}}>
+          <a className="navbar-brand page-scroll" href="#page-top">
             NursIA
           </a>{" "}
         </div>
@@ -28,17 +28,17 @@ export const Navigation = (props) => {
         >
           <ul className="nav navbar-nav navbar-right">
             <li>
-              <a href="#features" className="page-scroll" style={{color: "#F9B516"}}>
+              <a href="#features" className="page-scroll">
                 Features
               </a>
             </li>
             <li>
-              <a href="#about" className="page-scroll" style={{color: "#F9B516"}}>
+              <a href="#about" className="page-scroll">
                 About
               </a>
             </li>
             <li>
-              <a href="#services" className="page-scroll" style={{color: "#F9B516"}}>
+              <a href="#services" className="page-scroll">
                 Services
               </a>
             </li>
@@ -53,12 +53,12 @@ export const Navigation = (props) => {
               </a>
             </li> */}
             <li>
-              <a href="#team" className="page-scroll" style={{color: "#F9B516"}}>
+              <a href="#team" className="page-scroll">
                 Team
               </a>
             </li>
             <li>
-              <a href="#contact" className="page-scroll" style={{color: "#F9B516"}}>
+              <a href="#contact" className="page-scroll">
                 Contact
               </a>
             </li>
--- src/components/services.jsx
+++ src/components/services.jsx
@@ -14,13 +14,12 @@ export const Services = (props) => {
         <div className="row">
           {props.data
             ? props.data.map((d, i) => (
-                <div key={`${d.name}-${i}`} className="col-md-4" >
+                <div key={`${d.name}-${i}`} className="col-md-4">
                   {" "}
-                  <i className={d.icon} style={{backgroundColor:"#092677"}}></i>
+                  <i className={d.icon}></i>
                   <div className="service-desc">
                     <h3>{d.name}</h3>
                     <p>{d.text}</p>
-                    <p></p>
                   </div>
                 </div>
               ))
